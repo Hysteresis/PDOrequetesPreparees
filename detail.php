92c465772b6102ebbbbd75c5f00e2cd71bf57955
unknown <piballz@gmail.com>
2022-12-19 21:37:25 +0100

    add card et details

---
+++ detail.php
@@ -0,0 +1,52 @@
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Michroma&display=swap" rel="stylesheet">    
+    <link rel="stylesheet" href="style.css">
+    <title>details</title>
+</head>
+<body>
+
+    <h1>Detail</h1>
+    <?php
+    require_once('connect.php');
+        $id = $_GET['id'];
+        echo $id;
+        $stmt = $pdo->prepare ('SELECT * FROM monster WHERE id="' . $id . '"') ;
+        $stmt->execute(); 
+        $array = $stmt->fetchAll();
+    ?>
+
+<div class="container">
+                
+            <?php
+
+            // var_dump($array);
+            foreach($array as $value){
+            ?>
+                <div class="card-detail">
+                        <h2><?= $value['name'] ?> </h2>
+                        <p><?= $value['meta'] ?> </p>
+                        <br>
+                        <img class="img" src="<?=  $value['img_url'] ?>" alt="">
+                        <br>
+                        <p> Armor Class : <span class="gras"><?= $value['armor_class'] ?></span></p>
+                        <p> Hit Points : <span class="gras"><?= $value['hit_points'] ?></span></p>
+                        <p> speed : <span class="gras"><?= $value['speed'] ?></span></p>
+                        <p class="retour"><a href="index.php">Retour</a></p>
+                </div>
+            <?php 
+            }
+            
+            ?>
+
+            </div>
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,18 +1,18 @@
-<?php
-
-
-
-?>
-
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Michroma&display=swap" rel="stylesheet">    
+    <link rel="stylesheet" href="style.css">
+    <title>Liste</title>
 </head>
 <body>
+
+<h1>Liste des monsters</h1>
     <?php
     require_once('connect.php');
         // indiqué le chemin de votre fichier JSON, il peut s'agir d'une URL
@@ -21,14 +21,57 @@
 
         $type= "type";
         $tableau = $parsed_json ;
-        // var_dump($tableau);
 
-        foreach( $tableau as $l ){
-            echo "type : ".$l['meta']."</br>";
-            $stmt = $pdo->prepare ("INSERT INTO monster (type) VALUES( :type)") ;
-            $stmt->bindParam(':type', $l['meta'] );
-            $stmt->execute();         
-        }
+
+        //? inserer dans la bdd
+        // foreach( $tableau as $l ){
+        //     echo "type : ".$l['name']."</br>";
+        //     $stmt = $pdo->prepare ("INSERT INTO monster (name, meta, armor_class, hit_points, speed, img_url) VALUES( :name, :meta, :armor_class, :hit_points, :speed, :img_url)") ;
+        //     $stmt->bindParam(':name', $l['name'] );
+        //     $stmt->bindParam(':meta', $l['meta'] );
+        //     $stmt->bindParam(':armor_class', $l['Armor Class'] );
+        //     $stmt->bindParam(':hit_points', $l['Hit Points'] );
+        //     $stmt->bindParam(':speed', $l['Speed'] );
+        //     $stmt->bindParam(':img_url', $l['img_url'] );
+        //     $stmt->execute();        
+        // }
+        
+        //lire la bdd
+
+            $stmt = $pdo->prepare ("SELECT * FROM `monster`") ;
+            $stmt->execute(); 
+            $array = $stmt->fetchAll();
+            // var_dump($array);
+
+
+            ?>
+
+            <div class="container">
+                
+            <?php
+
+            // var_dump($array);
+            foreach($array as $value){
+            ?>
+                <div class="card">
+                        <h2><?= $value['name'] ?> </h2>
+                        <p><?= $value['meta'] ?> </p>
+                        <br>
+                        <img class="img" src="<?=  $value['img_url'] ?>" alt="">
+                        <br>
+                        <p> Armor Class : <span class="gras"><?= $value['armor_class'] ?></span></p>
+                        <p> Hit Points : <span class="gras"><?= $value['hit_points'] ?></span></p>
+                        <p> speed : <span class="gras"><?= $value['speed'] ?></span></p>
+                        <p class="detail"><a href="detail.php?id=<?= $value['id'] ?>">+ detail</a></p>
+                </div>
+            <?php 
+            }
+            
+            ?>
+
+            </div>
+            <?php
+        // echo  $stmt;
 
         // //! update
         // foreach( $tableau as $l ){
@@ -38,7 +81,7 @@
         //     $stmt->execute();         
         // }
 
-        // update monster set name = "Adult Black Dragon" where id = 3;
+
 
     ?>
 </body>
